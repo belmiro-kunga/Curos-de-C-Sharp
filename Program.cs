eaf9700fb008a867fd2ee5ef501218367e7c30d9
Belmiro M. Kunga <belmirokunga@gmail.com>
2021-10-04 23:33:35 +0100

    remove

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
 namespace Curos_de_C_Sharp
-{
-    class Program
-    {
+{class Program  {
         static void Main(string[] args)
         {
-            Console.Write("Hello World!");
+                    
+        Console.WriteLine("Olá Mundo");
+
+           
             
         }
     }
