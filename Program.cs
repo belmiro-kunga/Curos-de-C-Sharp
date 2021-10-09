4fafc4d6ed73609c29d85554f9c0df226d425de9
Belmiro M. Kunga <belmirokunga@gmail.com>
2021-10-09 22:16:52 +0100

    arry

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace Curos_de_C_Sharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           //Console.WriteLine("Hello World!");
+           Kunga(-19);
+             Kunga(1);
+               Kunga(15);
+                 Kunga(30);
         }
+
+        static void Kunga(int preco){
+
+            int precoaAbs =  Math.Abs(preco);
+            int precoFinal =  precoaAbs + (2 * precoaAbs);
+            Console.WriteLine("O Preço final é de: " + precoFinal);
+
+        } 
+
     }
 }
