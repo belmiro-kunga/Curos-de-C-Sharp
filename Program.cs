3b3295a49daff10bc8e5dd58fd14e283eae13093
Belmiro M. Kunga <belmirokunga@gmail.com>
2021-10-09 13:37:36 +0100

    funçoes

--- Program.cs
+++ Program.cs
@@ -1,18 +1,12 @@
 ﻿using System;
 
 namespace Curos_de_C_Sharp
-
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.ReadLine("Escre qual  uer coisa");
-      Console.WriteLine("Introduza um Nome:");
-
-      string userName = Console.ReadLine();
-
-      Console.WriteLine("O seu Nome é : " + userName);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
     }
-  }
 }
