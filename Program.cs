85f6ed1c1aca6f7bc29e264c1cf7faab1448aff4
Belmiro M. Kunga <belmirokunga@gmail.com>
2021-10-10 13:58:14 +0100

    metodo

--- Program.cs
+++ Program.cs
@@ -6,20 +6,25 @@ namespace Curos_de_C_Sharp
     {
         static void Main(string[] args)
         {
-           //Console.WriteLine("Hello World!");
-           Kunga(-19);
-             Kunga(1);
-               Kunga(15);
-                 Kunga(30);
+
+            //# Sobrecarga de método / Funçoes
+
+            int mynum1 = FuncaoInt(5, 7);
+            double mynum2 = FuncaoInt(4.2, 7.3);
+
+            Console.WriteLine("int " + mynum1);
+            Console.WriteLine("double " + mynum2);
+
+            
         }
 
-        static void Kunga(int preco){
+        static int FuncaoInt(int x, int y){
+            return x + y;
+        }
 
-            int precoaAbs =  Math.Abs(preco);
-            int precoFinal =  precoaAbs + (2 * precoaAbs);
-            Console.WriteLine("O Preço final é de: " + precoFinal);
 
-        } 
-
+        static double FuncaoInt(double x, double y){
+            return x + y;
+        }
     }
 }
