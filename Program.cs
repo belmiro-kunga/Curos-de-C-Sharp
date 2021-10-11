9849be45a2e31b3cc6a8bb37c017d5b8124bec19
Belmiro M. Kunga <belmirokunga@gmail.com>
2021-10-11 17:56:01 +0100

    ultimo

--- Program.cs
+++ Program.cs
@@ -4,40 +4,19 @@ namespace Curos_de_C_Sharp
 {
     class Program
     {
+
+
+        enum cor {preto, amarelo, vermelho, branco}
         static void Main(string[] args)
         {
-
-            //C# Condições Switch
-            int dia = 41;
-            switch(dia) 
-            {
-            case 1:
-                Console.WriteLine("Domingo");
-                break;
-            case 2:
-               Console.WriteLine("Segunda-feira");
-                break;
-            case 3:
-               Console.WriteLine("Terça-feira");
-                break;
-            case 4:
-               Console.WriteLine("Quarta-feira");
-                break;
-            case 5:
-               Console.WriteLine("Quinta-feira");
-                break;
-            case 6:
-               Console.WriteLine("Sexta-feira");
-                break;
-            case 7:
-               Console.WriteLine("Sabado");
-                break;
-            default:
-                Console.WriteLine("O teu calendario esta maluco");
-                break;
-            }
+         
+            //C# Enum
            
+           cor corPreferida = cor.branco;
+           cor CrPreferida = cor.amarelo;
 
+           Console.WriteLine(a)
+        
        
 
         }
