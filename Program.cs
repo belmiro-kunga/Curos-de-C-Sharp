dff6df291b1d9c6fc909154df4e5eb23710ce249
Belmiro M. Kunga <belmirokunga@gmail.com>
2021-10-12 23:06:18 +0100

    Criar um Menu
    Criar um Menu com  switch e enum

--- Program.cs
+++ Program.cs
@@ -4,17 +4,46 @@ namespace Curos_de_C_Sharp
 {
     class Program
     {
-
+        enum cor {Azul = 1, Amarelo, Castalho, Verde}
+        enum Opcao {Criar, Deletar, Editar, Listar, Atualizar}
 
        
         static void Main(string[] args)
         {
          
-            //C# Enum
-           
-          
+            //Criar Menu com enum
 
-           Console.WriteLine();
+            Console.WriteLine("Seleciona uma das opções: ");
+            Console.WriteLine("1-Criar\n2-Deletar\n3-Editar\n4-Listar\n5-Atualizar");
+
+           
+          int index = int.Parse(Console.ReadLine());
+          Opcao opcaoSelecionada = (Opcao)index;
+
+          switch(opcaoSelecionada ){
+            case Opcao.Criar:
+                Console.WriteLine("desejas Criar um Menu");
+                break;
+            case Opcao.Deletar:
+                Console.WriteLine("desejas Deletar um Menu");
+                break;
+            case Opcao.Editar:
+                Console.WriteLine("desejas Editar um Menue");
+                break;
+            case Opcao.Listar:
+                Console.WriteLine("desejasListar um Menu");
+                break;
+            case Opcao.Atualizar:
+                Console.WriteLine("desejas Atualiza um Menu");
+                break;
+            default:
+                Console.WriteLine("Esta Opção não Existe");
+                break;
+           
+            
+          }
+
+           Console.WriteLine(opcaoSelecionada);
         
        
 
